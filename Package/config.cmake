b8a8bd5518d7c7661d3c7e08f433ae78d055fa4c
Brandon Foster <Brandon.Foster4525@outlook.com>
2025-04-19 11:19:46 -0400

    restructure build and add linux clang support

--- Package/config.cmake
+++ Package/config.cmake
@@ -1,7 +1,10 @@
 include("${CMAKE_CURRENT_LIST_DIR}/lib/cmake/freetype/freetype-config.cmake")
 
-add_custom_target(FreeType-DLL-Copy
+add_custom_target(
+    FreeType-DLL-Copy
     DEPENDS "${CMAKE_CURRENT_LIST_DIR}/__run_always"
     COMMENT "FreeType DLL Copy"
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different "${CMAKE_CURRENT_LIST_DIR}/bin/freetype.dll" "${CMAKE_BINARY_DIR}/$<CONFIG>/bin/freetype.dll")
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different "${CMAKE_CURRENT_LIST_DIR}/bin/freetype.dll" "${CMAKE_BINARY_DIR}/$<CONFIG>/bin/freetype.dll"
+)
  add_dependencies(freetype FreeType-DLL-Copy)
+ 
