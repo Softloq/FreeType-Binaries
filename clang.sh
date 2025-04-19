40a3292078a3378c08295dda3bba8fc0328bfcc5
Brandon Foster <Brandon.Foster4525@outlook.com>
2025-04-19 07:04:28 -0400

    cleaned up msvc build and added clang build

---
+++ clang.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+(
+    source .env
+    SCRIPT_DIR=$(readlink -f $(dirname "$0"))
+    
+    cmake -S $SCRIPT_DIR/Prerequisite -B $SCRIPT_DIR/build/Prerequisite -DFREETYPE_VERSION="$FREETYPE_VERSION"
+    cmake --build $SCRIPT_DIR/build/Prerequisite
+
+    cmake -S $SCRIPT_DIR/build/Prerequisite/Downloads/freetype-VER-%FREETYPE_VERSION% -B $SCRIPT_DIR/build/Compile -G"Unix Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX="$SCRIPT_DIR/build/Package/freetype-%FREETYPE_VERSION%/freetype" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=23 -DCMAKE_CXX_STANDARD_REQUIRED=TRUE -DCMAKE_CXX_EXTENSIONS=OFF -DCMAKE_CXX_FLAGS_RELEASE="/MD" -DBUILD_SHARED_LIBS=ON
+    cmake --build $SCRIPT_DIR/build/Compile --config Release --target install -j$(nproc)
+
+    cmake -S $SCRIPT_DIR/Package -B $SCRIPT_DIR/build/Package -DFREETYPE_VERSION="$FREETYPE_VERSION" -DPACKAGE_NAME="freetype-linux-clang.zip"
+    cmake --build $SCRIPT_DIR/build/Package
+)
