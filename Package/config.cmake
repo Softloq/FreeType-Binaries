5c0e95e8550ad2e166d58daaedef7239a891b5b1
Brandon Foster <Brandon.Foster4525@outlook.com>
2025-04-28 09:25:38 -0400

    update binary build

--- Package/config.cmake
+++ Package/config.cmake
@@ -2,11 +2,10 @@ include("${CMAKE_CURRENT_LIST_DIR}/lib/cmake/freetype/freetype-config.cmake")
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
     add_custom_target(
-        FreeType-DLL-Copy
-        DEPENDS "${CMAKE_CURRENT_LIST_DIR}/__run_always"
-        COMMENT "FreeType DLL Copy"
+        Copy-FreeType-DLL
+        COMMENT "Copy FreeType DLL"
         COMMAND ${CMAKE_COMMAND} -E copy_if_different "${CMAKE_CURRENT_LIST_DIR}/bin/freetype.dll" "${CMAKE_BINARY_DIR}/$<CONFIG>/bin/freetype.dll"
     )
-    add_dependencies(freetype FreeType-DLL-Copy)
+    add_dependencies(freetype Copy-FreeType-DLL)
 endif()
  
